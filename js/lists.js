9d687b296976af9d23a481ae836f91ddba1784f6
BakaAwt <eric_awt@qq.com>
2017-06-18 12:33:04 +0800

    Add files via upload

---
+++ js/lists.js
@@ -0,0 +1,41 @@
+var t = {
+	playlist:[
+		{
+		  file: "http://awtupdateserve.oss-cn-shanghai.aliyuncs.com/tracks/01.mp3",
+		  thumb: "http://awtupdateserve.oss-cn-shanghai.aliyuncs.com/thumbs/01.jpg",
+		  trackName: "Dusk",
+		  trackArtist: "Tobu & Syndec",
+		  trackAlbum: "Single",
+		},
+		{
+		  file: "http://awtupdateserve.oss-cn-shanghai.aliyuncs.com/tracks/02.mp3",
+		  thumb: "http://awtupdateserve.oss-cn-shanghai.aliyuncs.com/thumbs/02.jpg",
+		  trackName: "Blank",
+		  trackArtist: "Disfigure",
+		  trackAlbum: "Single",
+		},
+		{
+		  file: "tracks/03.mp3",
+		  thumb: "http://awtupdateserve.oss-cn-shanghai.aliyuncs.com/thumbs/03.jpg",
+		  trackName: "Fade",
+		  trackArtist: "Alan Walker",
+		  trackAlbum: "Single",
+		},
+		{
+		  file: "http://awtupdateserve.oss-cn-shanghai.aliyuncs.com/Alan%20Walker%2CAlex%20Skrindo%20-%20Sky.mp3",
+		  thumb: "http://p4.music.126.net/Vx4M2DcQkR2J_fywgkDj5Q==/18586144557880349.jpg",
+		  trackName: "Sky",
+		  trackArtist: "Alan Walker,Alex Skrindo",
+		  trackAlbum: "Single",
+		},
+		{
+			file: "http://awtupdateserve.oss-cn-shanghai.aliyuncs.com/%CE%BC%27s%20-%20START%EF%BC%9ADASH%21%21.mp3",
+			thumb: "http://p4.music.126.net/F2rL7lYi3KcSpgym-O81pQ==/5888984278646723.jpg",
+			trackName: "START：DASH!!",
+			trackArtist: "μ's",
+			trackAlbum: "Single",
+		}
+	],
+	autoPlay:true
+};
+$(".jAudio--player").jAudio(t);
